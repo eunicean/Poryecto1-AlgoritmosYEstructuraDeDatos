648f008220615c3968cd77874bfac1592997c53b
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-03-14 08:03:01 -0600

    token done

--- proy1Algoritmos/src/Interprete/Token.java
+++ proy1Algoritmos/src/Interprete/Token.java
@@ -11,24 +11,39 @@ package Interprete;
 import java.util.regex.*;
 
 public class Token {
-	private String setqStatement = "";
-	private String additionStatement = "";
-	private String substractractionStatement = "";
-	private String multiplicationStatement = "";
-	private String divitionStatement = "";
-	private String quoteStatement = "";
-	private String defunStatement = "";
-	private String atomStatement = "";
-	private String listStatement = "";
-	private String equalStatement = "";
-	private String smallerThanStatement = "";
-	private String greaterThanStatement = "";
-	private String condStatement = "";
-	private String combStatement = "";
-	private String functionStatement = "";
-	private String defund2Statement = "";
+	//Arithmetic processes
+	private String additionStatement = "^[(][ ]*[+][ ]+([a-z]+|[0-9]+)[ ]+(([a-z]+|[0-9]+)[ ]*)*[)]$";
+	private String substractractionStatement = "^[(][ ]*[-][ ]+([a-z]+|[0-9]+)[ ]+(([a-z]+|[0-9]+)[ ]*)*[)]$";
+	private String multiplicationStatement = "^[(][ ]*[*][ ]+([a-z]+|[0-9]+)[ ]+(([a-z]+|[0-9]+)[ ]*)*[)]$";
+	private String divitionStatement = "^[(][ ]*[/][ ]+([a-z]+|[0-9]+)[ ]+(([a-z]+|[0-9]+)[ ]*)*[)]$";
+	//Quote
+	private String quoteStatement = "^[(][ ]*(quote |')+([+]|[-]|[*]|[/]|[(]|[)]|[0-9]|[a-z]|[defun]|[ ])*[)]$";
+	//Defun
+	private String defunStatement = "^[(][ ]*defun[ ]+([a-z]|[a-z,0-9]+)[ ]+([(]([a-z,0-9][ ]*)+[)][ ]*([(][ ]*([+]+|[-]+|[*]+|[/]+)[ ]+(([a-z]+[ ]([(].*[)])+)|([0-9]+[ ]([(].*[)])+)|([(].*[)])+|(([(].*[)])+[ ][0-9]+)|(([(].*[)])+[ ][a-z]+)|([a-z]+|[0-9]+)[ ]+(([a-z]+|[0-9]+)[ ]*))[ ]*[)]))[)]$";
+	private String defund2Statement = "^[(][ ]*defun[ ]+([a-z]|[a-z,0-9]+)[ ]+([(]([a-z,0-9][ ]*)+[)][ ]*([(]cond.*[)]))[)]$";
+	//Setq
+	private String setqStatement = "^[(][ ]*setq[ ]+[a-z]+[ ]+[0-9]+[ ]*[)]$";
+	//Predicados
+	private String atomStatement = "^[(][ ]*atom[ ]+((((\\\"[a-z]\\\")+|[0-9]+|(NIL)+|(T)+|('[0-9]+))[ ]*)|[']([(]+[ ]*(((\\\"[a-z]\\\")+|[0-9]+|(NIL)+|(T)+)[ ]*)+[)]))[)]$";
+	private String listStatement = "^[(][ ]*list[ ]*(((\\\"[a-z]\\\")*|[0-9]*|(NIL)*|(T)*)[ ]*)+[)]$";
+	private String equalStatement = "^[(][ ]*equal[ ]+[0-9]+[ ]+[0-9]+[ ]*[)]$";
+	private String smallerThanStatement = "^[(][ ]*[<][ ]+([a-z]+|[0-9]+)+[ ]+([a-z]+|[0-9]+)+[ ]*[)]$";
+	private String greaterThanStatement = "[(][ ]*[>][ ]+([a-z]+|[0-9]+)+[ ]+([a-z]+|[0-9]+)+[ ]*[)]$";
+	//Cond
+	private String condStatement = "^[(]cond [(]([(].*[)])[)]$";
+	//step of parameters
+	private String combStatement = "^[(][ ]*([+]+|[-]+|[*]+|[/]+)[ ]+(([a-z]+[ ]([(].*[)])+)|([0-9]+[ ]([(].*[)])+)|([(].*[)])+|(([(].*[)])+[ ][0-9]+)|(([(].*[)])+[ ][a-z]+))[ ]*[)]$";
+	private String functionStatement = "^[(][ ]*([a-z]|[a-z,0-9]+)[ ]+([a-z]+|[0-9]+)[ ]*(([a-z]+|[0-9]+)[ ]*)*[)]$";
+	
+	String[] Tokens = {additionStatement,substractractionStatement,multiplicationStatement,divitionStatement,quoteStatement,defunStatement,defund2Statement,setqStatement,atomStatement,listStatement,equalStatement,smallerThanStatement,greaterThanStatement,condStatement,combStatement,functionStatement};
 	
 	
+	/**
+	 * Method that validates if the code inserted match a determinate pattern.
+	 * @param ToCompare String that contains the pattern.
+	 * @param code String that contains the code that it wants to compare and match.
+	 * @return Boolean with true if it matches, false if not.
+	 */
 	public boolean validatePattern(String ToCompare, String code) {
 		Pattern pattern = Pattern.compile(ToCompare, Pattern.CASE_INSENSITIVE);
 		Matcher matcher = pattern.matcher(code);
@@ -36,4 +51,13 @@ public class Token {
 		return matchFound;
 	}
 
+	
+	public int getToken(String code) {
+		for(int i = 0; i < Tokens.length ; i++) {
+			if(validatePattern(Tokens[i],code)) {
+				return i;
+			}
+		}
+		return -1;
+	}
 }
