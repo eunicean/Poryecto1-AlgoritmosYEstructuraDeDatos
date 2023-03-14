e02a3f61aeb82d843c27b60cdf687b57b6432622
Samantha Bhor <audreybhorguzman@gmail.com>
2023-03-14 14:28:18 -0600

    Commit of operations interface of interpreter

---
+++ proy1Algoritmos/src/Operations/IOperationsfile.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ * Conetenedor (interfaz) de clases operacionales
+ *
+ *@author Eunice Mata,Samantha Bhor
+ */
+
+
+public interface IOperationsfile{
+	
+	public String performOperation();
+	public void addResults(String key, String result);
+	public String getKey();
+	public String getResult();
+	
+	
+}
