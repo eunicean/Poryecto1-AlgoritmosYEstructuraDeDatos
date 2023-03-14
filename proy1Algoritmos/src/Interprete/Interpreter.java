eb4addb6a709ca3fba5bd0074a4a31f07cb7e32a
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-03-14 09:02:12 -0600

    changes

---
+++ proy1Algoritmos/src/Interprete/Interpreter.java
@@ -0,0 +1,13 @@
+/**
+ * Object that simulates the interpreter of the language LISP 
+ * @author Eunice Mata - 21231
+ * @author Samantha Bhor - 
+ */
+
+package Interprete;
+
+import java.util.*;
+
+public class Interpreter {
+
+}
