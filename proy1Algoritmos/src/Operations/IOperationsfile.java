7302f556f0b0854d5aeb154cf4145dc1e7aa2e0f
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-03-14 22:18:02 -0600

    Inteface for file operations done [Propuesta de Samantha]

---
+++ proy1Algoritmos/src/Operations/IOperationsfile.java
@@ -0,0 +1,8 @@
+package Operations;
+
+public interface IOperationsfile {
+	public String maketheoperations();
+	public void Results(String key, String result);
+	public String getKey();
+	public String getResult();
+}
