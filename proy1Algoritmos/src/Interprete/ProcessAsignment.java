275b92a1aee57f76b4bad73a22a56a2f871fc8f2
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-03-15 16:22:46 -0600

    redoing f assignment

---
+++ proy1Algoritmos/src/Interprete/ProcessAsignment.java
@@ -0,0 +1,11 @@
+/**
+ * Object that simulates the assignment of the procees given by the token :D
+ * @author Eunice Mata - 21231
+ * @author Samantha Bhor - 22545
+ * This mf doesn't want to f wooooooork
+ */
+package Interprete;
+
+public class ProcessAsignment {
+
+}
