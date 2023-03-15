97d797ac9e02edf55b4e1b28f81f25c66e469583
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-03-15 17:48:44 -0600

    addition and substraction

--- proy1Algoritmos/src/Interprete/Interpreter.java
+++ proy1Algoritmos/src/Interprete/Interpreter.java
@@ -84,16 +84,21 @@ public class Interpreter {
 		return null;
 	}
 	
+	/**
+	 * Method that assigns the process of addition
+	 * @param code String with the code that contains the numbers or variables to do the addition
+	 * @return object type IOperationsfile that has the information of the process
+	 */
 	public IOperationsfile additionProcessAssignment(String code) {
 		int total = 0;
 		Pattern pattern = Pattern.compile("([a-z]+|[0-9]+)", Pattern.CASE_INSENSITIVE); //
-		Pattern patternNum = Pattern.compile("([0-9]+)", Pattern.CASE_INSENSITIVE); //
-		Pattern patternVar = Pattern.compile("([a-z]+)", Pattern.CASE_INSENSITIVE); //
+		Pattern patternNumber = Pattern.compile("([0-9]+)", Pattern.CASE_INSENSITIVE); //
+		Pattern patternVariable = Pattern.compile("([a-z]+)", Pattern.CASE_INSENSITIVE); //
 		Matcher matcher = pattern.matcher(code);
 		
 		while (matcher.find()) {
-	    	Matcher matcherNum = patternNum.matcher(matcher.group().trim());
-	    	Matcher matcherVar = patternVar.matcher(matcher.group().trim());
+	    	Matcher matcherNum = patternNumber.matcher(matcher.group().trim());
+	    	Matcher matcherVar = patternVariable.matcher(matcher.group().trim());
 	    	
 	    	if(matcherNum.lookingAt()) {
 	    		
@@ -104,15 +109,118 @@ public class Interpreter {
 		    		int valor = Variables.get(matcher.group());
 		    		total += valor;
 		    	}else {
-		    		ErrorProcess errorResult = new ErrorProcess();
-					errorResult.Results("VARIABLE ERROR", "Variable invalida.");
-					return errorResult;
+		    		ErrorProcess error = new ErrorProcess();
+					error.Results("VARIABLE ERROR", "Invalid variable");
+					return error;
 		    	}
 	    	}
 	    }
 		
-		ArithmeticProcess miResult = new ArithmeticProcess();
-	    miResult.Results(" suma ", "" + total);
-	    return miResult;
+		IOperationsfile result = new ArithmeticProcess();
+		result.Results("suma", String.valueOf(total));
+	    return result;
+	}
+	/**
+	 * Method that assigns the process of substraction
+	 * @param code String with the code that contains the numbers or variables to do the substraction
+	 * @return object type IOperationsfile that has the information of the process
+	 */
+	public IOperationsfile substractionProcessAssignment(String code) {
+		int total = 0, counter = 0;
+		Pattern pattern = Pattern.compile("([a-z]+|[0-9]+)", Pattern.CASE_INSENSITIVE); //
+		Pattern patternNumber = Pattern.compile("([0-9]+)", Pattern.CASE_INSENSITIVE); //
+		Pattern patternVariable = Pattern.compile("([a-z]+)", Pattern.CASE_INSENSITIVE); //
+		Matcher matcher = pattern.matcher(code);
+		
+		while (matcher.find()) {
+	    	if(counter == 0) {
+	    		Matcher matcherNum = patternNumber.matcher(matcher.group().trim());
+		    	Matcher matcherVar = patternVariable.matcher(matcher.group().trim());
+		    	
+		    	if(matcherNum.lookingAt()) {
+		    		
+		    		total = Integer.parseInt(matcher.group().trim());
+		    		
+		    	}else if(matcherVar.lookingAt()) {
+		    		if(Variables.containsKey(matcher.group())){
+			    		int valor = Variables.get(matcher.group());
+			    		total = valor;
+			    	}else {
+			    		ErrorProcess error = new ErrorProcess();
+			    		error.Results("VARIABLE ERROR", "Variable invalida.");
+						return error;
+			    	}
+		    	}
+	    	}else {	    		
+	    		Matcher matcherNum = patternNumber.matcher(matcher.group().trim());
+		    	Matcher matcherVar = patternVariable.matcher(matcher.group().trim());
+		    	
+		    	if(matcherNum.lookingAt()) {
+		    		
+		    		total -= Integer.parseInt(matcher.group().trim());
+		    		
+		    	}else if(matcherVar.lookingAt()) {
+		    		if(Variables.containsKey(matcher.group())){
+			    		int valor = Variables.get(matcher.group());
+			    		total -= valor;
+			    	}else {
+			    		ErrorProcess errorResult = new ErrorProcess();
+						errorResult.Results("VARIABLE ERROR", "Variable invalida.");
+						return errorResult;
+			    	}
+		    	}
+	    	}
+	    	counter ++;
+	    }
+		IOperationsfile result = new ArithmeticProcess();
+		result.Results("suma", String.valueOf(total));
+	    return result;
+	}
+	
+	public IOperationsfile multiplicationProcessAssignment(String code) {
+		return null;
+	}
+	
+	public IOperationsfile divitionProcessAssignment(String code) {
+		return null;
+	}
+	
+	public IOperationsfile variableAssignmentProcess(String code) {
+		return null;
+	}
+	
+	public IOperationsfile equalProcessAssignment(String code) {
+		return null;
+	}
+	
+	public IOperationsfile greaterThanAssignmentProcess(String code) {
+		return null;
+	}
+	public IOperationsfile lesserThanAssignmentProcess(String code) {
+		return null;
+	}
+	public IOperationsfile listAssignmentProcess(String code) {
+		return null;
+	}
+	public IOperationsfile atomAssignmentProcess(String code) {
+		return null;
+	}
+	public IOperationsfile quoteAssignmentProcess(String code) {
+		return null;
+	}
+	public IOperationsfile combAssignmentProcess(String code) {
+		return null;
+	}
+	public IOperationsfile combAssignmentProcessResult(String code) {
+		return null;
+	}
+	public IOperationsfile defunAssignmentProcess(String code) {
+		return null;
+	}
+	public IOperationsfile functionAssignmentProcess(String code) {
+		return null;
+	}
+	public IOperationsfile condAssignmentProcess(String code) {
+		return null;
 	}
 }
