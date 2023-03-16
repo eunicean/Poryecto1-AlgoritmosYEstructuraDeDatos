664f5d428fd01aa6075d5a1a22ad6682ad8f2380
Eunice Mata <72279865+eunicean@users.noreply.github.com>
2023-03-15 20:13:16 -0600

    hope it works

--- proy1Algoritmos/src/Interprete/Interpreter.java
+++ proy1Algoritmos/src/Interprete/Interpreter.java
@@ -342,31 +342,360 @@ public class Interpreter {
 		Pattern patternVar = Pattern.compile("([a-z]+)", Pattern.CASE_INSENSITIVE); //
 		Matcher matcher = pattern.matcher(code);
 		
-		
+		 while (matcher.find()) {
+		    	Matcher matcherNum = patternNum.matcher(matcher.group().trim());
+		    	Matcher matcherVar = patternVar.matcher(matcher.group().trim());
+		    	
+		    	if(counter == 1) {	    	
+		    		if(matcherNum.lookingAt()) {		    		
+		    			firstVariable = Integer.parseInt(matcher.group().trim());
+			    	}else if(matcherVar.lookingAt()) {
+			    		if(Variables.containsKey(matcher.group())){
+			    			firstVariable = Variables.get(matcher.group());
+				    	}else {
+				    		IOperationsfile error = new ErrorProcess();
+				    		error.Results("VARIABLE", "Invalid variable");
+							return error;
+				    	}
+			    	}
+		    	}else {
+		    		if(matcherNum.lookingAt()) {		    		
+		    			secondVariable = Integer.parseInt(matcher.group().trim());
+			    	}else if(matcherVar.lookingAt()) {
+			    		if(Variables.containsKey(matcher.group())){
+			    			secondVariable = Variables.get(matcher.group());
+				    	}else {
+				    		IOperationsfile error = new ErrorProcess();
+				    		error.Results("VARIABLE", "Invalid variable");
+							return error;
+				    	}
+			    	}
+		    	}
+		    	counter ++;
+		    }
+		    
+		    if(firstVariable > secondVariable) {
+		    	r = "TRUE";
+		    }else {
+		    	r = "NIL";
+		    }
 		IOperationsfile result = new PredicatesProcess();
-		result.Results("Equal", r);
+		result.Results("Mayor que", r);
 		return result;
 	}
 	public IOperationsfile lesserThanAssignmentProcess(String code) {
-		return null;
+		int firstVariable = 0,secondVariable = 0, counter = 1;
+		String r = "";
+		Pattern pattern = Pattern.compile("([a-z]+|[0-9]+)", Pattern.CASE_INSENSITIVE); //
+		Pattern patternNum = Pattern.compile("([0-9]+)", Pattern.CASE_INSENSITIVE); //
+		Pattern patternVar = Pattern.compile("([a-z]+)", Pattern.CASE_INSENSITIVE); //
+		Matcher matcher = pattern.matcher(code);
+		
+		while (matcher.find()) {
+	    	Matcher matcherNum = patternNum.matcher(matcher.group().trim());
+	    	Matcher matcherVar = patternVar.matcher(matcher.group().trim());
+	    	
+	    	if(counter == 1) {	    	
+	    		if(matcherNum.lookingAt()) {		    		
+	    			firstVariable = Integer.parseInt(matcher.group().trim());
+		    	}else if(matcherVar.lookingAt()) {
+		    		if(Variables.containsKey(matcher.group())){
+		    			firstVariable = Variables.get(matcher.group());
+			    	}else {
+			    		IOperationsfile error = new ErrorProcess();
+			    		error.Results("VARIABLE", "Invalid variable");
+			    		return error;
+			    	}
+		    	}
+	    	}else {
+	    		if(matcherNum.lookingAt()) {		    		
+	    			secondVariable = Integer.parseInt(matcher.group().trim());
+		    	}else if(matcherVar.lookingAt()) {
+		    		if(Variables.containsKey(matcher.group())){
+		    			secondVariable = Variables.get(matcher.group());
+			    	}else {
+			    		IOperationsfile error = new ErrorProcess();
+			    		error.Results("VARIABLE", "Invalid variable");
+			    		return error;
+			    	}
+		    	}
+	    	}
+	    	counter ++;
+	    }
+	    
+	    if(firstVariable < secondVariable) {
+	    	r = "TRUE";
+	    }else {
+	    	r = "NIL";
+	    }
+		
+		IOperationsfile result = new PredicatesProcess();
+		result.Results("Menor que", r);
+		return result;
 	}
 	public IOperationsfile listAssignmentProcess(String code) {
-		return null;
+		String r="(";
+		int cont = 0;
+		Pattern pattern = Pattern.compile("(('[a-z]')+|[0-9]+|(NIL)+|([ ]+T)+)", Pattern.CASE_INSENSITIVE); //
+		Matcher matcher = pattern.matcher(code);
+		
+		while(matcher.find()) {
+			
+			if(cont==0) {
+				r += String.valueOf(matcher.group().trim());
+			}else {
+				r += " "+String.valueOf(matcher.group().trim());
+			}
+
+			cont++;
+		}
+		
+		if(cont == 0) {
+			r = "NIL";
+		}else {
+			r += ")";
+		}
+		
+		IOperationsfile result = new PredicatesProcess();
+		result.Results("list", r);
+		return result;
+		
 	}
 	public IOperationsfile atomAssignmentProcess(String code) {
-		return null;
+		String r="";
+		boolean atom = true;
+		Pattern patternAtom = Pattern.compile("^[(][ ]*atom[ ]+(((\"[a-z]\")+|[0-9]+|(NIL)+|(T)+|('[0-9]+))[ ]*)[)]$", Pattern.CASE_INSENSITIVE); //
+		Pattern patternConsp = Pattern.compile("^[(][ ]*atom[ ]+[']([(]+[ ]*(((\"[a-z]\")+|[0-9]+|(NIL)+|(T)+)[ ]*)+[)])[)]$", Pattern.CASE_INSENSITIVE); //
+		Matcher matcherAtom = patternAtom.matcher(code);
+		Matcher matcherConsp = patternConsp.matcher(code);
+		
+		while(matcherAtom.find()) {
+			atom = true;
+		}
+		while(matcherConsp.find()) {
+			atom = false;
+		}
+		if(atom) {
+			r = "TRUE"; }
+		else {
+			r = "NIL"; }
+		
+		IOperationsfile result = new PredicatesProcess();
+		result.Results("atom", r);
+		return result;
 	}
 	public IOperationsfile quoteAssignmentProcess(String code) {
-		return null;
+	    String ToReplace = "", r = "";
+		Pattern pattern = Pattern.compile("(quote |')+", Pattern.CASE_INSENSITIVE); //
+	    Matcher matcher = pattern.matcher(code);
+	    
+	    if(matcher.find())
+	    	ToReplace = matcher.group().trim();
+	    r = code.replace(ToReplace, "");
+	    r = r.substring(1, r.length()-1);
+	    IOperationsfile result = new PredicatesProcess();
+		result.Results("de quote", r);
+		return result;
 	}
-	public IOperationsfile combAssignmentProcess(String code) {
-		return null;
+	public String combVerifyingProcess(String code) {
+		
+		code = code.substring(1,code.length()-1);
+		
+		int countSubs = 0, countDiv = 0, total = 0;
+		String operator = null;
+		Pattern pattern = Pattern.compile("(([a-z,0-9]+)+|(([ ][(]).*[)])+|([+]+|[-]+|[*]+|[/]+)+)", Pattern.CASE_INSENSITIVE);
+		Pattern patternNumber = Pattern.compile("([0-9]+)", Pattern.CASE_INSENSITIVE);
+		Pattern patternVariable = Pattern.compile("([a-z]+)", Pattern.CASE_INSENSITIVE); //
+		Pattern patternParentesis = Pattern.compile("(([(]).*[)])", Pattern.CASE_INSENSITIVE);
+		Pattern patternOperation = Pattern.compile("([+]+|[-]+|[*]+|[/]+)", Pattern.CASE_INSENSITIVE);
+		Matcher matcher = pattern.matcher(code);
+		
+		while (matcher.find()) {
+			
+	    	Matcher matcherOp = patternOperation.matcher(matcher.group().trim());
+
+	    	if(matcherOp.lookingAt()) {
+	    		operator = matcherOp.group();
+	    		if(operator.equals("*") || operator.equals("/")) {
+	    			total = 1;
+	    		}
+	    	}else{
+	    		switch (operator) {
+		    		case "+":
+		    			Matcher matcherNum = patternNumber.matcher(matcher.group().trim());
+		    			Matcher matcherParentesis = patternParentesis.matcher(matcher.group().trim());
+		    			Matcher matcherVar = patternVariable.matcher(matcher.group().trim());
+		    	    	if(matcherNum.lookingAt()) {	
+		    	    		total += Integer.parseInt(matcher.group().trim());
+		    	    	}else if(matcherVar.lookingAt()){
+		    	    		if(Variables.containsKey(matcher.group())){
+		    		    		int valor = Variables.get(matcher.group());
+		    		    		total += valor;
+		    		    	}else {
+		    		    		return "ERROR";
+		    		    	}
+		    	    	}else if(matcherParentesis.lookingAt()) {
+		    	    		try {
+		    	    			total += Integer.parseInt(combVerifyingProcess(matcherParentesis.group()));
+		    	    		}catch(Exception e) {
+		    	    			return "ERROR";
+		    	    		}
+		    	    	}
+		    			break;
+		    		case "-":
+		    			matcherNum = patternNumber.matcher(matcher.group().trim());
+		    			matcherParentesis = patternParentesis.matcher(matcher.group().trim());
+		    			matcherVar = patternVariable.matcher(matcher.group().trim());
+		    			if(countSubs == 0) {
+		    				if(matcherNum.lookingAt()) {
+		    		    		total = Integer.parseInt(matcher.group().trim());
+		    		    	}else if(matcherVar.lookingAt()){
+			    	    		if(Variables.containsKey(matcher.group())){
+			    		    		int valor = Variables.get(matcher.group());
+			    		    		total = valor;
+			    		    	}else {
+			    					return "ERROR";
+			    		    	}
+			    	    	}else if(matcherParentesis.lookingAt()) {
+			    	    		try {
+			    	    			total = Integer.parseInt(combVerifyingProcess(matcherParentesis.group()));
+			    	    		}catch(Exception e) {
+			    	    			return "ERROR";
+			    	    		}
+			    	    	}
+		    				countSubs ++;
+		    			}else {
+		    				matcherNum = patternNumber.matcher(matcher.group().trim());
+		    				matcherParentesis = patternParentesis.matcher(matcher.group().trim());
+		    				matcherVar = patternVariable.matcher(matcher.group().trim());
+		    		    	
+		    		    	if(matcherNum.lookingAt()) {
+		    		    		total -= Integer.parseInt(matcher.group().trim());
+		    		    	}else if(matcherVar.lookingAt()){
+			    	    		if(Variables.containsKey(matcher.group())){
+			    		    		int valor = Variables.get(matcher.group());
+			    		    		total -= valor;
+			    		    	}else {
+			    					return "ERROR";
+			    		    	}
+			    	    	}else if(matcherParentesis.lookingAt()) {
+			    	    		try {
+			    	    			total -= Integer.parseInt(combVerifyingProcess(matcherParentesis.group()));
+			    	    		}catch(Exception e) {
+			    	    			return "ERROR";
+			    	    		}
+			    	    	}
+		    			}
+		    			break;
+		    		case "*":
+		    			matcherNum = patternNumber.matcher(matcher.group().trim());
+		    			matcherParentesis = patternParentesis.matcher(matcher.group().trim());
+		    			matcherVar = patternVariable.matcher(matcher.group().trim());
+		    	    	if(matcherNum.lookingAt()) {	
+		    	    		total *= Integer.parseInt(matcher.group().trim());
+		    	    	}else if(matcherVar.lookingAt()){
+		    	    		if(Variables.containsKey(matcher.group())){
+		    		    		int valor = Variables.get(matcher.group());
+		    		    		total *= valor;
+		    		    	}else {
+		    					return "ERROR";
+		    		    	}
+		    	    	}else if(matcherParentesis.lookingAt()) {
+		    	    		try {
+		    	    			total *= Integer.parseInt(combVerifyingProcess(matcherParentesis.group()));
+		    	    		}catch(Exception e) {
+		    	    			return "ERROR";
+		    	    		}
+		    	    	}
+		    			break;
+		    		case "/":
+		    			matcherNum = patternNumber.matcher(matcher.group().trim());
+		    			matcherParentesis = patternParentesis.matcher(matcher.group().trim());
+		    			matcherVar = patternVariable.matcher(matcher.group().trim());
+		    			
+		    			if(countDiv == 0) {
+		    				if(matcherNum.lookingAt()) {
+		    		    		total = Integer.parseInt(matcher.group().trim());
+		    		    	}else if(matcherVar.lookingAt()){
+			    	    		if(Variables.containsKey(matcher.group())){
+			    		    		int valor = Variables.get(matcher.group());
+			    		    		total = valor;
+			    		    	}else {
+			    					return "ERROR";
+			    		    	}
+			    	    	}else if(matcherParentesis.lookingAt()) {
+			    	    		try {
+			    	    			total = Integer.parseInt(combVerifyingProcess(matcherParentesis.group()));
+			    	    		}catch(Exception e) {
+			    	    			return "ERROR";
+			    	    		}
+			    	    	}
+		    				countDiv ++;
+		    			}else {
+		    				matcherNum = patternNumber.matcher(matcher.group().trim());
+		    				matcherParentesis = patternParentesis.matcher(matcher.group().trim());
+		    				matcherVar = patternVariable.matcher(matcher.group().trim());
+		    		    	
+		    		    	if(matcherNum.lookingAt()) {
+		    		    		total /= Integer.parseInt(matcher.group().trim());
+		    		    	}else if(matcherVar.lookingAt()){
+			    	    		if(Variables.containsKey(matcher.group())){
+			    		    		int valor = Variables.get(matcher.group());
+			    		    		total /= valor;
+			    		    	}else {
+			    					return "ERROR";
+			    		    	}
+			    	    	}else if(matcherParentesis.lookingAt()) {
+			    	    		try {
+			    	    			total /= Integer.parseInt(combVerifyingProcess(matcherParentesis.group()));
+			    	    		}catch(Exception e) {
+			    	    			return "ERROR";
+			    	    		}
+			    	    	}
+		    			}
+		    		break;
+		    	}
+	    	}
+		}
+		return String.valueOf(total);
 	}
 	public IOperationsfile combAssignmentProcessResult(String code) {
-		return null;
+		//it needs a process to verify to simplify the process
+		String total = combVerifyingProcess(code);
+		if(total.equals("ERROR")) {
+			IOperationsfile error = new ErrorProcess();
+			error.Results("VARIABLE", "Invalid variable");
+			return error;
+		}else {			
+			IOperationsfile Result = new ArithmeticProcess();
+			Result.Results("comb", total);
+			return Result;	
+		}
 	}
 	public IOperationsfile defunAssignmentProcess(String code) {
-		return null;
+	    String functionName = "",functionValue = "";
+		Pattern pattern = Pattern.compile("(defun[ ]+([a-z]|[a-z,0-9])+)", Pattern.CASE_INSENSITIVE); //
+	    Matcher matcher = pattern.matcher(code);
+
+	    if (matcher.find()) {
+	    	functionName = matcher.group().replaceFirst("defun", " ").trim();
+	    }
+	     
+	    Pattern patternValue = Pattern.compile("([(]([a-z,0-9][ ]*)+[)][ ]*([(][ ]*([+]+|[-]+|[*]+|[/]+)[ ]+(([a-z]+[ ]([(].*[)])+)|([0-9]+[ ]([(].*[)])+)|([(].*[)])+|(([(].*[)])+[ ][0-9]+)|(([(].*[)])+[ ][a-z]+)|([a-z]+|[0-9]+)[ ]+(([a-z]+|[0-9]+)[ ]*))[ ]*[)]))", Pattern.CASE_INSENSITIVE); //
+	    Pattern patternValue2 = Pattern.compile("([(]([a-z,0-9][ ]*)+[)][ ]*([(]cond.*[)]))", Pattern.CASE_INSENSITIVE); //
+	    matcher = patternValue.matcher(expresion.substring(0, expresion.length()-1));
+	    Matcher matcher2 = patternValue2.matcher(expresion.substring(0, expresion.length()-1));
+	     
+	    if (matcher.find()) {
+	    	functionValue = matcher.group();
+	    }else if(matcher2.find()) {
+	    	functionValue = matcher2.group();
+	    }
+	     Functions.put(functionName, functionValue); //adding the function to the map for funtions
+
+	    IOperationsfile Result = new ArithmeticProcess();
+		Result.Results(functionName, functionValue);
+		return Result;	
 	}
 	public IOperationsfile functionAssignmentProcess(String code) {
 		return null;
